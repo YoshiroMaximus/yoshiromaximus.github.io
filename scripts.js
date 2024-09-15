e13b29fd79666723a29996c381621c471d9e7997
yoshiro <76180677+YoshiroMaximus@users.noreply.github.com>
2024-09-15 14:00:57 -0700

    oh

--- scripts.js
+++ scripts.js
@@ -3,8 +3,5 @@ const body = document.body;
 
 themeToggleButton.addEventListener('click', () => {
     const isDarkMode = body.classList.toggle('dark-mode');
-    body.classList.toggle('light-mode', !isDarkMode);
-    
-    // Update the button text
     themeToggleButton.textContent = isDarkMode ? 'Light Mode' : 'Dark Mode';
 });
