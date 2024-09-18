84a53220bae421df798ebbbcf4990592d5b028ca
yoshiro <76180677+YoshiroMaximus@users.noreply.github.com>
2024-09-18 15:52:08 -0700

    tetris

--- README.md
+++
@@ -1,1 +0,0 @@
-# yoshiromaximus.github.io
--- scripts.js
+++
@@ -1,7 +0,0 @@
-const themeToggleButton = document.getElementById('theme-toggle');
-const body = document.body;
-
-themeToggleButton.addEventListener('click', () => {
-    const isDarkMode = body.classList.toggle('dark-mode');
-    themeToggleButton.textContent = isDarkMode ? 'Light Mode' : 'Dark Mode';
-});
