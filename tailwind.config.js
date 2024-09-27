21064cd0e13a70fcfca288a37b589b32a5a2412b
yoshiro <76180677+YoshiroMaximus@users.noreply.github.com>
2024-09-26 18:47:52 -0700

    tailwind?

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class', // Use 'class' to enable dark mode toggling
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {
       colors: {
-        // Customize your color palette
         darkBg: '#1e293b', // Dark mode background
         lightBg: '#f3f4f6', // Light mode background
         textLight: '#ffffff', // Light text for dark mode
